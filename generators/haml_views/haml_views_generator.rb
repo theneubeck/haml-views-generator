a290fde976d59b92979d3bb179a1d606d2be64fe
Jens Neubeck <jens.neubeck@newsline.se>
2009-02-27 10:11:58 +0100

    Added files.

---
+++ generators/haml_views/haml_views_generator.rb
@@ -0,0 +1,95 @@
+class HamlViewsGenerator < Rails::Generator::NamedBase
+  default_options :skip_timestamps => false, :skip_migration => false, :force_plural => false
+
+  attr_reader   :controller_name,
+                :controller_class_path,
+                :controller_file_path,
+                :controller_class_nesting,
+                :controller_class_nesting_depth,
+                :controller_class_name,
+                :controller_underscore_name,
+                :controller_singular_name,
+                :controller_plural_name
+  alias_method  :controller_file_name,  :controller_underscore_name
+  alias_method  :controller_table_name, :controller_plural_name
+
+  def initialize(runtime_args, runtime_options = {})
+    super
+
+    if @name == @name.pluralize && !options[:force_plural]
+      logger.warning "Plural version of the model detected, using singularized version.  Override with --force-plural."
+      @name = @name.singularize
+    end
+
+    @controller_name = @name.pluralize
+
+    base_name, @controller_class_path, @controller_file_path, @controller_class_nesting, @controller_class_nesting_depth = extract_modules(@controller_name)
+    @controller_class_name_without_nesting, @controller_underscore_name, @controller_plural_name = inflect_names(base_name)
+    @controller_singular_name=base_name.singularize
+    if @controller_class_nesting.empty?
+      @controller_class_name = @controller_class_name_without_nesting
+    else
+      @controller_class_name = "#{@controller_class_nesting}::#{@controller_class_name_without_nesting}"
+    end
+  end
+
+  def manifest
+    record do |m|
+      # Check for class naming collisions.
+      m.class_collisions(class_name)
+
+      # Controller, helper, views, test and stylesheets directories.
+      m.directory(File.join('app/models', class_path))
+      m.directory(File.join('app/controllers', controller_class_path))
+      m.directory(File.join('app/helpers', controller_class_path))
+      m.directory(File.join('app/views', controller_class_path, controller_file_name))
+
+      for action in scaffold_views
+        m.template(
+          "view_#{action}.html.haml",
+          File.join('app/views', controller_class_path, controller_file_name, "#{action}.html.haml")
+        )
+      end
+
+
+      # m.template(
+      #   'controller.rb', File.join('app/controllers', controller_class_path, "#{controller_file_name}_controller.rb")
+      # )
+
+    end
+  end
+
+  protected
+    # Override with your own usage banner.
+    def banner
+      "Usage: #{$0} haml_views ModelName"
+    end
+
+    def add_options!(opt)
+      opt.separator ''
+      opt.separator 'Options:'
+      opt.on("--skip-timestamps",
+             "Don't add timestamps to the migration file for this model") { |v| options[:skip_timestamps] = v }
+      opt.on("--skip-migration",
+             "Don't generate a migration file for this model") { |v| options[:skip_migration] = v }
+      opt.on("--force-plural",
+             "Forces the generation of a plural ModelName") { |v| options[:force_plural] = v }
+    end
+
+    def scaffold_views
+      %w[ index show new edit _form ]
+    end
+
+    def model_name
+      class_name.demodulize
+    end
+
+    def attributes
+      entity_class = class_eval(model_name)
+      attributes = entity_class.columns_hash.keys.reject { |k| ["updated_at", "created_at", "id"].include? k }
+      @attributes ||= attributes.map do |attribute|
+        Rails::Generator::GeneratedAttribute.new(attribute.to_s, entity_class.columns_hash[attribute.to_s].type)
+      end
+    end
+    
+end
